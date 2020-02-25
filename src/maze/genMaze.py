7d1c11cbca4f5a1c6f5096d6475669054e0985fc
huma3 <huma3@mcmaster.ca>
2020-02-25 14:17:41 -0500

    Began coding

--- src/maze/genMaze.py
+++ src/maze/genMaze.py
@@ -1,3 +1,9 @@
-from pygame.locals import pygame *
+import os
 import pygame
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
+pygame.init()
 
+class maze:
+    def__init__(self):
+        self.M = 10
+        self.N = 10
