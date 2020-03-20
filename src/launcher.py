b6bbba6714a5481b646aaaf27905580df7ec6b74
Mengxi <leim5@mcmaster.ca>
2020-03-20 19:38:28 -0400

    update launcher

--- src/launcher.py
+++ src/launcher.py
@@ -1,13 +1,14 @@
 # Launcher module of the program. This is the module that will be responsible for launching the mini-games
 # By Mengxi (William) Lei
-# Created 2020/02/11, Last Modified 2020/02/11
+# Created 2020/02/11, Last Modified 2020/03/20
 
 # Importation
 import pygame
+import thorpy
 from maze import MazeGame
 
 # Class Definition
-class Launcher :
+class Launcher:
 
     #Static Variables
     gameTitle = ["Maze", "Flappy", "Pong"]
@@ -22,27 +23,57 @@ class Launcher :
     # Displaying the launcher
     @staticmethod
     def displayLauncher():
-        # Declare Variables
-        index = None
-        game = None
-        userInput = None
 
-        # Main loop
-        while True:
-            print("Please enter the number representing the option you wish to select:")
-            for index, game in enumerate(Launcher.gameTitle):
-                print("\t" + str(index+1) + ": " + game)
-            print("\t0: Exit\n")
-            userInput = int(input())
-            if userInput == 0:
-                print("\nExiting\n\n")
-                break
-            elif userInput == 1:
-                Launcher.games[userInput-1].on_execute()
-            elif userInput >= 2 and userInput <=3:
-                print("\nRunning " + Launcher.gameTitle[userInput-1] + "\n\n")
-            else:
-                print("\n\nInvalid number, please enter a valid number.\n\n")
+        # Declare Variables
+        index = None                 #int
+        game = None                  #string
+        application = None           #Application(thorpy)
+        buttons = [None, None, None] #Clickable(thorpy) Array
+        quitButtons = None           #Clickable(thorpy)
+        buttonGroup = None           #Group(thorpy)
+        menu = None                  #Menu(thorpy)
+        
+        # Create GUI and set theme
+        application = thorpy.Application(size=(1280,800), caption="Mini-Games")
+        thorpy.theme.set_theme("human")
+
+        # Create buttons
+        for index, game in enumerate(Launcher.gameTitle):
+            buttons[index] = thorpy.Clickable(game)
+            buttons[index].user_func = Launcher.launchGame
+            buttons[index].user_params = {"game": index}
+            buttons[index].set_size((100, 50))
+            buttons[index].set_main_color((102,204,255))
+        quitButtons = thorpy.Clickable("Exit")
+        quitButtons.user_func = thorpy.functions.quit_menu_func
+        
+        # Format the buttons
+        buttonGroup = thorpy.make_group(buttons)
+
+        # Set background
+        background = thorpy.Background(color=(255,255,255), elements=[buttonGroup, quitButtons])
+        thorpy.store(background)
+
+        # Create menu and display
+        menu = thorpy.Menu(background)
+        menu.play()
+
+        # Exiting
+        application.quit()
+
+
+
+##################################################################################################################################
+
+
+
+    # Launch the given game
+    @staticmethod
+    def launchGame(game):
+        if game == 0:
+            Launcher.games[game].on_execute()
+        elif game >= 1 and game <=2:
+            print("\nRunning " + Launcher.gameTitle[game] + "\n\n")
 
 
 
